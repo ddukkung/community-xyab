8d41e8c62c77dd6fe87e76ef0c7ee909f508c9c6
ddukkung <gjznn3657@gmail.com>
2022-03-22 21:37:08 +0900

    Initial commit

---
+++ src/test/java/community/xyab/XyabApplicationTests.java
@@ -0,0 +1,13 @@
+package community.xyab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
